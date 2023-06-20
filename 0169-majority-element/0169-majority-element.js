df19c05fbc10ffb3a01129860b4c94ea5a5f7d69
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-06-20 14:36:43 +0900

    Time: 72 ms (40.76%), Space: 43.7 MB (64.01%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,17 +3,13 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
+    const n = Math.floor(nums.length / 2);
     const obj = {};
+
+    if (nums.length <= 2) return nums[0];
+    
     for (let i = 0; i < nums.length; i++) {
-        if (obj[nums[i]] !== undefined) {
-            obj[nums[i]] += 1;
-        } else {
-            obj[nums[i]] = 1;
-        }
-    }
-    for (const num in obj) {
-       if (obj[num] > nums.length / 2) {
-           return num;
-       }
+        obj[nums[i]] = obj[nums[i]] + 1 || 1;
+        if (obj[nums[i]] > n) return nums[i];
     }
 };
