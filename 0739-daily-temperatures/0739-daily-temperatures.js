029a6a3c194e2113183f34189bad880aca7fba66
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-07-18 23:53:32 +0900

    Time: 288 ms (63.28%), Space: 66.2 MB (64.95%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+  let stack = [];
+ let result = new Array(temperatures.length).fill(0);
+  for(let i=temperatures.length-1; i >= 0; i--){
+      while(stack.length && temperatures[i] >= temperatures[stack[stack.length-1]]){
+       stack.pop();
+      }
+      if(stack.length){
+          result[i] = stack[stack.length-1] - i;   
+      }
+      stack.push(i)
+  }
+  return result;
+};
+
