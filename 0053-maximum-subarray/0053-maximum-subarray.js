135137b6f857f91fc4e5584db1c35f463b5a2ba2
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-07-19 15:10:15 +0900

    Time: 94 ms (12.74%), Space: 50.2 MB (64.24%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = function(nums) {
+  let max = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+    max = Math.max(nums[i], max);
+  }
+  return max;
+};
