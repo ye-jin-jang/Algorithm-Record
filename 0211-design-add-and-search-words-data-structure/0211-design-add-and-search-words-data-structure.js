ccada723e3df32d030a5d06d498ea1c7dcf81e0f
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-07-22 20:22:31 +0900

    Time: 802 ms (95.04%), Space: 98.3 MB (81.21%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.js
@@ -0,0 +1,52 @@
+
+var WordDictionary = function() {
+  this.trie = {};
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function(word) {
+  let root = this.trie;
+  
+  for (let i = 0; i < word.length; i++) {
+    if (root[word[i]] == null) {
+      root[word[i]] = {};
+    }
+    root = root[word[i]];
+  }
+  
+  root.isEnd = true;
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function(word) {
+  return this.dfs(word, 0, this.trie);
+};
+
+WordDictionary.prototype.dfs = function (word, index, root) {
+  if (index === word.length) return root.isEnd === true;
+  
+  if (word[index] === ".") {
+    for (const key in root) {
+      if (this.dfs(word, index + 1, root[key])) return true;
+    }
+  }
+
+  if (root[word[index]] != null) {
+    return this.dfs(word, index + 1, root[word[index]]);
+  }
+  
+  return false;
+}
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
