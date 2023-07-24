9b31170bec2e8fdae3efd2e9b0e69182b40f02cf
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-07-24 16:28:06 +0900

    Time: 65 ms (17.69%), Space: 42 MB (31.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,10 +2,10 @@
  * @param {number} n
  * @return {number}
  */
-let result = [];
+const result = [];
 var climbStairs = function(n) {
-    if (n < 4) return n;
-    if (result[n] !== undefined) return result[n];
-    result[n] = climbStairs(n - 1) + climbStairs(n - 2);
-    return result[n];
+  if (n < 4) return n;
+  if (result[n]) return result[n];
+  result[n] = climbStairs(n - 1) + climbStairs(n - 2);
+  return result[n];
 };
