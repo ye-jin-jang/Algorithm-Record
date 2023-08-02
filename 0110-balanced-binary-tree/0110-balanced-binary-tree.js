6e105a9098b8ae316ae66ea42edd6947a54de144
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-08-02 19:09:26 +0900

    Time: 62 ms (94.94%), Space: 47.1 MB (74.34%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -11,14 +11,15 @@
  * @return {boolean}
  */
 const isBalanced = function(root) {
-    let dfs = function(node) {
-      if (!node) return 0;
-      let left = 1 + dfs(node.left);
-      let right = 1 + dfs(node.right);
-      if (Math.abs(left - right) > 1) return Infinity;
-      
-      return Math.max(left, right);
-    }
-    
-    return dfs(root) === Infinity ? false : true;
+  function dfs(root) {
+    if (!root) return 0;
+  
+    let left = 1 + dfs(root.left);
+    let right = 1 + dfs(root.right);
+    if (Math.abs(left - right) > 1) return Infinity;
+
+    return Math.max(left, right);
+  }
+  
+  return dfs(root) === Infinity ? false : true;
 }
