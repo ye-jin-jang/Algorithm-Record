43e340a50fbaeb4e74cc53ef71847aef0f551f2b
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-08-05 22:35:17 +0900

    Time: 70 ms (99.67%), Space: 47.6 MB (51.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -7,7 +7,7 @@ const coinChange = function(coins, amount) {
   const dp = Array(amount + 1).fill(Infinity);
   dp[0] = 0;
   
-  for (let coin of coins) {
+  for (const coin of coins) {
     for (let i = coin; i <= amount; i++) {
       dp[i] = Math.min(dp[i], dp[i - coin] + 1);
     }
