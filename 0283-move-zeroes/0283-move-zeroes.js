d842db77d21a706dbd9c9c379f6a6e8d1a6e6e8b
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-08-11 02:59:30 +0900

    Time: 74 ms (91.47%), Space: 46.5 MB (62.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,20 +2,14 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-  let low = 0;
-  let high = low + 1;
-
-  while (high <= nums.length - 1) {
-    if (nums[low] !== 0) {
-      low++;
-      high++;
-    } else {
-      if (nums[high] !== 0) {
-        [nums[low], nums[high]] = [nums[high], nums[low]];
-        low++;
-      }
-      high++;
+const moveZeroes = function(nums) {
+  let gap = 0;
+    for(let i=0; i<nums.length; i++) {
+        if(nums[i] === 0) {
+            gap++;
+        }
+        else {
+            [nums[i-gap], nums[i]] = [nums[i], nums[i-gap]]
+        }
     }
-  }
 };
