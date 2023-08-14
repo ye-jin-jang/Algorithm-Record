443cf4297cf26a5584defc725db5031fe04b4d9d
ye-jin-jang <78009532+ye-jin-jang@users.noreply.github.com>
2023-08-14 16:53:43 +0900

    Time: 78 ms (33.87%), Space: 44.6 MB (34.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
+const missingNumber = function(nums) {
   const sortNums = nums.sort((a,b) => a - b);
   const numsCount = sortNums.length;
   for (let i = 0; i <= numsCount; i++) {
